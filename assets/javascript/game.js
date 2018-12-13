14923311b7fc1ff17df4d818db4d3a639dadbd82
Babyhacker36 <louiscarterjr29@gmail.com>
2018-12-13 06:55:42 -0500

    final file push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,175 +1,64 @@
-Skip to content
+var words = ["jon", "cersei", "tyrion", "sansa", "missandei", "lyanna"]
 
-Search or jump to…
-
-Pull requests
-Issues
-Marketplace
-Explore
-@Babyhacker36 Sign out
-0
-0 0 Rubyrgill / Word - Guess - Game
-Code Issues 0 Pull requests 0 Projects 0 Wiki Insights
-Word - Guess - Game / assets / javascript / game.js
-102 eb79 on Jun 24
-@Rubyrgill Rubyrgill saved placeholder images, and updated links
-
-244 lines(212 sloc) 7.23 KB
-
-//VARIABLES
-var words = ["arthur", "rugrats", "thesimpsons", "scoobydoo", "spongebob", "dannyphantom", "teentitans"]
-
-//Empty variables to store values later
 var randomWord = "";
-var lettersOfWord = []
+var wordsLetters = []
 var blanks = 0;
 var blanksAndCorrect = [];
 var wrongGuess = [];
-
-//Counter Variables
 var wins = 0;
 var losses = 0;
 var guessesRemaining = 9;
 
+//GAME START
 
-
-// ALL FUNCTIONS
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-
-//__________________________________________________________
-//GAME START FUNCTION
-//__________________________________________________________
 function Game() {
-    //computer generates random word from words array
     randomWord = words[Math.floor(Math.random() * words.length)];
-
-    // split the individual word into separate arrays, and store in new array 
-    lettersOfWord = randomWord.split("");
-
-    //store length of word in blanks, for later use
-    blanks = lettersOfWord.length;
-
-    //creating a loop to generate "_" for each letter in array stored in blanks
+    wordsLetters = randomWord.split("");
+    blanks = wordsLetters.length;
     for (var i = 0; i < blanks; i++) {
         blanksAndCorrect.push("_");
     }
-
-    //showing the "_" within HTML
     document.getElementById("currentword").innerHTML = "  " + blanksAndCorrect.join("  ");
-
-    //console logging 
     console.log(randomWord);
-    console.log(lettersOfWord)
+    console.log(wordsLetters)
     console.log(blanks)
     console.log(blanksAndCorrect)
 }
 
+var words = ["jon", "cersei", "tyrion", "sansa", "missandei", "lyanna"]
 
-//__________________________________________________________
-//AUDIO FUNCTION
-//__________________________________________________________
+// swap of the images
+function imgswap() {
 
-//variables for audio function
-var a = document.getElementById("arthur");
-var r = document.getElementById("rugrats");
-var simpsons = document.getElementById("simpsons");
-var scoobydoo = document.getElementById("scoobydoo");
-var spongebob = document.getElementById("spongebob");
-var danny = document.getElementById("danny");
-var teent = document.getElementById("teent");
-
-
-function aud() {
-    //Arthur Audio & Image
-    //---------------------------
     if (randomWord === words[0]) {
-        scoobydoo.pause();
-        spongebob.pause();
-        danny.pause();
-        teent.pause();
-        simpsons.pause();
-        r.pause();
-        a.play();
-        document.getElementById("image").src = "./assets/images/arther.gif";
-    }
-    //Rugrats Audio & Image
-    //---------------------------
-    else if (randomWord === words[1]) {
-        scoobydoo.pause();
-        spongebob.pause();
-        danny.pause();
-        teent.pause();
-        simpsons.pause();
-        a.pause();
-        r.play();
-        document.getElementById("image").src = "./assets/images/rugrats.gif";
-    }
-    //Simpsons Audio & Image
-    //---------------------------
-    else if (randomWord === words[2]) {
-        scoobydoo.pause();
-        spongebob.pause();
-        danny.pause();
-        teent.pause();
-        r.pause();
-        a.pause();
-        simpsons.play();
-        document.getElementById("image").src = "./assets/images/simpsons.gif";
-    }
-    //Scooby-doo Audio & Image
-    //---------------------------
-    else if (randomWord === words[3]) {
-        spongebob.pause();
-        danny.pause();
-        teent.pause();
-        simpsons.pause();
-        r.pause();
-        a.pause();
-        scoobydoo.play();
-        document.getElementById("image").src = "./assets/images/scooby.gif";
-    }
-    //Spongebob Audio & Image
-    //---------------------------
-    else if (randomWord === words[4]) {
-        danny.pause();
-        teent.pause();
-        simpsons.pause();
-        r.pause();
-        a.pause();
-        scoobydoo.pause();
-        spongebob.play();
-        document.getElementById("image").src = "./assets/images/spongebob.gif";
-    }
-    //Danny Audio & Image
-    //---------------------------
-    else if (randomWord === words[5]) {
-        spongebob.pause();
-        teent.pause();
-        simpsons.pause();
-        r.pause();
-        a.pause();
-        scoobydoo.pause();
-        danny.play();
-        document.getElementById("image").src = "./assets/images/danny.gif";
-    }
-    //Teen Titans Audio & Image
-    //---------------------------
-    else if (randomWord === words[6]) {
-        spongebob.pause();
-        danny.pause();
-        simpsons.pause();
-        r.pause();
-        a.pause();
-        scoobydoo.pause();
-        teent.play();
-        document.getElementById("image").src = "./assets/images/teen.gif";
-    }
+        document.getElementById("image").src = "./assets/images/jon.gif";
+        document.getElementById("hiddenH5").style.visibility = "hidden";
+        // document.getElementById('names ').innerHTML += 'Jon Snow';
+    } else if (randomWord === words[1]) {
+        document.getElementById("image").src = "./assets/images/cersei.gif";
+        document.getElementById("hiddenH5").style.visibility = "hidden";
+        // document.getElementById('names').innerHTML += 'Cersei Lannister';
+    } else if (randomWord === words[2]) {
+        document.getElementById("image").src = "./assets/images/tyrion.gif";
+        document.getElementById("hiddenH5").style.visibility = "hidden";
+        // document.getElementById('names').innerHTML += 'Tyrion Lannister';
+    } else if (randomWord === words[3]) {
+        document.getElementById("image").src = "./assets/images/sansa.gif";
+        document.getElementById("hiddenH5").style.visibility = "hidden";
+        // document.getElementById('names').innerHTML += 'Sansa Stark';
+    } else if (randomWord === words[4]) {
+        document.getElementById("image").src = "./assets/images/missandei.gif";
+        document.getElementById("hiddenH5").style.visibility = "hidden";
+        // document.getElementById('names').innerHTML += 'Missandei';
+    } else if (randomWord === words[5]) {
+        document.getElementById("image").src = "./assets/images/lyanna.gif";
+        document.getElementById("hiddenH5").style.visibility = "hidden";
+        // document.getElementById('names').innerHTML += 'Lyanna Mormont';
+    };
 };
 
-//__________________________________________________________
-//RESET FUNCTION
-//__________________________________________________________
+//RESET 
+
 function reset() {
     guessesRemaining = 9;
     wrongGuess = [];
@@ -177,57 +66,45 @@ function reset() {
     Game()
 }
 
-//__________________________________________________________
-//CHECK LETTERS/COMPARE FUNCTION
-//__________________________________________________________
-
-//If/Else, to see if letter selected matches random word
 function checkLetters(letter) {
     var letterInWord = false;
-    //if the generated randomword is equal to the letter entered... then variable is true
     for (var i = 0; i < blanks; i++) {
         if (randomWord[i] == letter) {
             letterInWord = true;
         }
     }
-    //if letterInWord (false)
     if (letterInWord) {
-        //check each letter to see if it matches word
         for (var i = 0; i < blanks; i++) {
             if (randomWord[i] == letter) {
                 blanksAndCorrect[i] = letter;
             }
         }
-    }
-    //otherwise, push the incorrect guess in the wrong guesses section, and reduce remaining guesses
-    else {
+    } else {
         wrongGuess.push(letter);
         guessesRemaining--;
     }
     console.log(blanksAndCorrect);
 }
 
-//__________________________________________________________
 //FINAL COMPLETE FUNCTION
-//__________________________________________________________
 
 //check to see if player won...
 function complete() {
     console.log("wins:" + wins + "| losses:" + losses + "| guesses left:" + guessesRemaining)
 
-    //if WON...then alert, play audio, display image and reset new round
-    if (lettersOfWord.toString() == blanksAndCorrect.toString()) {
+    //WON
+    if (wordsLetters.toString() == blanksAndCorrect.toString()) {
         wins++;
-        aud()
+        imgswap()
         reset()
         //display wins on screen
         document.getElementById("winstracker").innerHTML = " " + wins;
 
-        //if LOST...then alert and reset new round
+        //LOST
     } else if (guessesRemaining === 0) {
         losses++;
         reset()
-        document.getElementById("image").src = "./assets/images/try-again.png"
+        document.getElementById("image").src = "./assets/images/tryagain.png"
         document.getElementById("losstracker").innerHTML = " " + losses;
     }
     //display losses on screen && guesses remaining countdown
@@ -236,38 +113,14 @@ function complete() {
 }
 
 
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-//_____________________________________________________
-// EXECUTE CODE 
-//_____________________________________________________
-
 //call start game function
 Game()
 
-//check for keyup, and convert to lowercase then store in guesses
+
 document.onkeyup = function (event) {
     var guesses = String.fromCharCode(event.keyCode).toLowerCase();
-    //check to see if guess entered matches value of random word
     checkLetters(guesses);
-    //process wins/loss 
     complete();
-    //store player guess in console for reference 
     console.log(guesses);
-
-    //display/store incorrect letters on screen
     document.getElementById("playerguesses").innerHTML = "  " + wrongGuess.join(" ");
 }
-
-© 2018 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-Press h to open a hovercard with more details.
